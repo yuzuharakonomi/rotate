9541807b6d9f2a3f0f050d9160297993d82175cc
Yuzuhara Konomi <yuzuharakonomi@icloud.com>
2026-02-08 23:28:42 +0900

    use a translation table

--- src/rotate/rotate.py
+++ src/rotate/rotate.py
@@ -5,18 +5,12 @@ def rotate(text: str, rotation: int) -> str:
     if not text.isalpha() or not text.isascii():
         raise ValueError()
 
-    total_alphabets = len(string.ascii_lowercase)
-    rotated_text = ""
-    for character in text:
-        offset = string.ascii_lowercase.find(character.lower())
-        if offset == -1:
-            raise ValueError()
-
-        final_rotation = (offset + rotation) % total_alphabets
-
-        if character.isupper():
-            rotated_text += string.ascii_uppercase[final_rotation]
-        else:
-            rotated_text += string.ascii_lowercase[final_rotation]
-
-    return rotated_text
+    normalized_rotation = rotation % 26
+    translation_table = str.maketrans(
+        string.ascii_lowercase + string.ascii_uppercase,
+        string.ascii_lowercase[normalized_rotation:]
+        + string.ascii_lowercase[:normalized_rotation]
+        + string.ascii_uppercase[normalized_rotation:]
+        + string.ascii_uppercase[:normalized_rotation],
+    )
+    return text.translate(translation_table)
