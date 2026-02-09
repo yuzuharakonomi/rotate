77fbe76a2dafbf70742d5f478dbcdc874078c42b
Yuzuhara Konomi <yuzuharakonomi@icloud.com>
2026-02-09 22:33:15 +0900

    add test cases

--- tests/test_main.py
+++ tests/test_main.py
@@ -16,3 +16,15 @@ class TestRotate:
 
     def test_rotate_minus_27(self):
         assert rotate("Hello", -27) == "Gdkkn"
+
+    def test_rotate_0(self):
+        assert rotate("Hello", 0) == "Hello"
+
+    def test_rotate_26(self):
+        assert rotate("Hello", 26) == "Hello"
+
+    def test_text_with_non_alphabetical_character(self):
+        assert rotate("Hello, World!", 1) == "Ifmmp, Xpsme!"
+
+    def test_text_with_non_ascii_character(self):
+        assert rotate("Helloはこんにちは", 1) == "Ifmmpはこんにちは"
